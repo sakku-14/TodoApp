223a902368cce1ff369b9e413adc8503fc9b1541
Atsuhiro.uehara <try.again.31.06@gmail.com>
2023-08-19 16:01:33 +0900

    #18 更新用のボトムシートを共通のボトムシートを使うよう変更

--- lib/View/edit_bottom_sheet_view.dart
+++ lib/View/edit_bottom_sheet_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/View/common_bottom_sheet_view.dart';
 
-class EditBottomSheetView extends StatelessWidget {
-  const EditBottomSheetView({super.key});
+class EditBottomSheetView extends ConsumerWidget {
+  const EditBottomSheetView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final commonButtonSheetViewKey =
+        GlobalObjectKey<CommonBottomSheetViewState>(context);
+    // var notifier = ref.read(editBottomSheetViewModelProvider.notifier);
     var screenSize = MediaQuery.of(context).size;
-    String? todoTitle = '初期値';
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
@@ -27,15 +31,18 @@ class EditBottomSheetView extends StatelessWidget {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: screenSize.width * 0.04),
+                    fontSize: screenSize.width * 0.05),
               ),
             ),
             SizedBox(
               width: screenSize.width * 0.3,
               child: TextButton(
                 onPressed: () {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('$todoTitle')));
+                  var inputInfo =
+                      commonButtonSheetViewKey.currentState?.getInputInfo();
+                  if (inputInfo == null) return;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(inputInfo.$1.toString())));
                 },
                 onLongPress: () {},
                 child: const Text('更新'),
@@ -43,22 +50,7 @@ class EditBottomSheetView extends StatelessWidget {
             ),
           ],
         ),
-        // TODO:23.8.17:A.Uehara:ここから下は共通のダイアログを使う
-        Container(
-          margin: EdgeInsets.all(screenSize.width * 0.1),
-          child: SizedBox(
-            width: screenSize.width * 0.8,
-            child: TextField(
-              onChanged: (text) {
-                todoTitle = text;
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'タイトル',
-              ),
-            ),
-          ),
-        ),
+        CommonBottomSheetView(key: commonButtonSheetViewKey),
       ],
     );
   }
