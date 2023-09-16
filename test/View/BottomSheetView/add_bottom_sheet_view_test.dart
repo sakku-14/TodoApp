a2f908019399419fe4719495aac604c3ebe8074c
Atsuhiro.uehara <try.again.31.06@gmail.com>
2023-09-16 20:58:24 +0900

    #24 add_bottom_sheet_view_testが全て通ることを確認できたため、コメントアウトを外した

--- test/View/BottomSheetView/add_bottom_sheet_view_test.dart
+++ test/View/BottomSheetView/add_bottom_sheet_view_test.dart
@@ -22,66 +22,66 @@ main() {
     );
   }
 
-  // testWidgets('登録用ボトムシートの表示項目が正しいこと', (tester) async {
-  //   // AddBottomSheetViewを生成
-  //   await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
-  //   await tester.pumpAndSettle();
-  //
-  //   // 各項目が正しいこと
-  //   // ボトムシートヘッダー部分
-  //   expect(find.text('Todoの登録'), findsOneWidget);
-  //   expect(find.text('キャンセル'), findsOneWidget);
-  //   expect(find.text('登録'), findsOneWidget);
-  //   // Todoタイトル入力用Field
-  //   expect(find.byType(TextField), findsOneWidget);
-  //   // 緊急度選択肢
-  //   expect(find.text('緊急度'), findsOneWidget);
-  //   expect(emergencyInput, findsOneWidget);
-  //   // 重要度選択肢
-  //   expect(find.text('重要度'), findsOneWidget);
-  //   expect(priorityInput, findsOneWidget);
-  //   // ステータス選択肢
-  //   expect(find.text('ステータス'), findsOneWidget);
-  //   expect(statusInput, findsOneWidget);
-  // });
-  //
-  // testWidgets('キャンセルボタンを押下するとボトムシートが閉じること', (tester) async {
-  //   // AddBottomSheetViewを生成
-  //   await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
-  //
-  //   // キャンセルテキストボタンをタップ
-  //   await tester.tap(cancelButtonInput);
-  //   await tester.pumpAndSettle();
-  //
-  //   // ボトムシートが存在しないこと
-  //   expect(addBottomSheetViewInput, findsNothing);
-  // });
-  //
-  // testWidgets('タイトルが空の状態で登録ボタンを押下してもボトムシートが閉じないこと', (tester) async {
-  //   // AddBottomSheetViewを生成
-  //   await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
-  //
-  //   // 登録テキストボタンをタップ
-  //   await tester.tap(addButtonInput);
-  //   await tester.pumpAndSettle();
-  //
-  //   // ボトムシートが存在すること
-  //   expect(addBottomSheetViewInput, findsOneWidget);
-  // });
-  //
-  // testWidgets('タイトルが入力された状態で登録ボタンを押下するとボトムシートが閉じること', (tester) async {
-  //   // AddBottomSheetViewを生成
-  //   await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
-  //
-  //   // titleを入力
-  //   await tester.enterText(titleInput, 'text');
-  //   await tester.pumpAndSettle();
-  //
-  //   // 登録テキストボタンをタップ
-  //   await tester.tap(addButtonInput);
-  //   await tester.pumpAndSettle();
-  //
-  //   // ボトムシートが存在しないこと
-  //   expect(addBottomSheetViewInput, findsNothing);
-  // });
+  testWidgets('登録用ボトムシートの表示項目が正しいこと', (tester) async {
+    // AddBottomSheetViewを生成
+    await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
+    await tester.pumpAndSettle();
+
+    // 各項目が正しいこと
+    // ボトムシートヘッダー部分
+    expect(find.text('Todoの登録'), findsOneWidget);
+    expect(find.text('キャンセル'), findsOneWidget);
+    expect(find.text('登録'), findsOneWidget);
+    // Todoタイトル入力用Field
+    expect(find.byType(TextField), findsOneWidget);
+    // 緊急度選択肢
+    expect(find.text('緊急度'), findsOneWidget);
+    expect(emergencyInput, findsOneWidget);
+    // 重要度選択肢
+    expect(find.text('重要度'), findsOneWidget);
+    expect(priorityInput, findsOneWidget);
+    // ステータス選択肢
+    expect(find.text('ステータス'), findsOneWidget);
+    expect(statusInput, findsOneWidget);
+  });
+
+  testWidgets('キャンセルボタンを押下するとボトムシートが閉じること', (tester) async {
+    // AddBottomSheetViewを生成
+    await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
+
+    // キャンセルテキストボタンをタップ
+    await tester.tap(cancelButtonInput);
+    await tester.pumpAndSettle();
+
+    // ボトムシートが存在しないこと
+    expect(addBottomSheetViewInput, findsNothing);
+  });
+
+  testWidgets('タイトルが空の状態で登録ボタンを押下してもボトムシートが閉じないこと', (tester) async {
+    // AddBottomSheetViewを生成
+    await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
+
+    // 登録テキストボタンをタップ
+    await tester.tap(addButtonInput);
+    await tester.pumpAndSettle();
+
+    // ボトムシートが存在すること
+    expect(addBottomSheetViewInput, findsOneWidget);
+  });
+
+  testWidgets('タイトルが入力された状態で登録ボタンを押下するとボトムシートが閉じること', (tester) async {
+    // AddBottomSheetViewを生成
+    await tester.pumpWidget(addBottomSheetView(const AddBottomSheetView()));
+
+    // titleを入力
+    await tester.enterText(titleInput, 'text');
+    await tester.pumpAndSettle();
+
+    // 登録テキストボタンをタップ
+    await tester.tap(addButtonInput);
+    await tester.pumpAndSettle();
+
+    // ボトムシートが存在しないこと
+    expect(addBottomSheetViewInput, findsNothing);
+  });
 }
