ffb99812e6d789ac13f301da24246a050cf9dcfc
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-10-14 16:17:00 +0900

    #30 メインのロボットクラス追加＆アプリ表示メソッドも追加し、それを叩くよう修正

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,13 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:todo_app/main.dart' as app;
 
+import '../test/robot/robot.dart';
+
 void main() {
   var binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('DBが空の状態でスタート', (WidgetTester tester) async {
     // DBのSetUpをやるよ
-    app.main();
-    await tester.pumpAndSettle();
+    final r = Robot(tester);
+    await r.pumpMyApp();
 
     // await todoRobot.slideTodo();
 
@@ -20,8 +22,8 @@ void main() {
 
   testWidgets('DBにデータがある状態でスタート', (WidgetTester tester) async {
     // DBのSetUpをやるよ
-    app.main();
-    await tester.pumpAndSettle();
+    final r = Robot(tester);
+    await r.pumpMyApp();
 
     // await todoRobot.slideTodo();
 
