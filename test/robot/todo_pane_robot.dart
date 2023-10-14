f5bbb4588a0097fec45bcfb9337024ea6895e2b5
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-10-14 17:12:13 +0900

    #30 ロボットにステータスタブ切り替える処理追加

--- test/robot/todo_pane_robot.dart
+++ test/robot/todo_pane_robot.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app/model/model.dart';
 
 class TodoPaneRobot {
   const TodoPaneRobot(this.tester);
@@ -6,7 +8,14 @@ class TodoPaneRobot {
   final WidgetTester tester;
 
   Future<void> scrollToBottom() async {}
-  Future<void> changeStateTab() async {}
+
+  Future<void> changeStatusTabTo(TodoStatus status) async {
+    final tab = find.widgetWithText(Tab, status.statusName);
+    expect(tab, findsOneWidget);
+    await tester.tap(tab);
+    await tester.pumpAndSettle();
+  }
+
   Future<void> slideTodo() async {}
   Future<void> pressUpdateButton() async {}
   Future<void> pressDeleteButton() async {}
