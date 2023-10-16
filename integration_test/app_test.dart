8cb8187015d103a8760dfef27aff0cdee0e4c4fc
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-10-17 08:41:29 +0900

    #30 DBファイルのコピー元をintegration_test/sources内になるよう修正

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -15,7 +15,7 @@ const String dbFilePath = 'integration_test/resources/$dbFileName';
 void main() {
   final _ = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  Future<File> crateFilePath(String fileName) async {
+  Future<File> getDbFile(String fileName) async {
     var dbFilePath = '';
 
     if (Platform.isAndroid) {
@@ -29,12 +29,10 @@ void main() {
 
   Future<void> deleteFile() async {
     try {
-      final file = await crateFilePath(dbFileName);
+      final file = await getDbFile(dbFileName);
       if (file.existsSync()) {
         file.deleteSync(); // ファイルを削除
         debugPrint('ファイルを削除しました: todo_database.db');
-      } else {
-        debugPrint('指定されたファイルが存在しません: todo_database.db');
       }
     } catch (e) {
       debugPrint('ファイルの削除中にエラーが発生しました: $e');
@@ -43,25 +41,32 @@ void main() {
 
   Future<void> deployFile() async {
     // 環境変数からプロジェクトのルートディレクトリを取得
-    const relativePath = String.fromEnvironment('projectRoot');
+    const projectRootDirPath = String.fromEnvironment('projectRoot');
 
     // テスト用DB ファイルの絶対パスを取得
-    final absoluteFilePath = File(join(relativePath, dbFilePath));
-    if (!absoluteFilePath.existsSync()) {
-      debugPrint('コピー元のファイルが存在しません: $absoluteFilePath');
+    final sourceFile = File(join(projectRootDirPath, dbFilePath));
+    if (!sourceFile.existsSync()) {
+      debugPrint('コピー元のファイルが存在しません: $sourceFile');
       return;
     }
 
-    File destinationFilePath = await crateFilePath(dbFileName);
+    File destFile = await getDbFile(dbFileName);
+    print('コピー元ファイルパス:${sourceFile.path}');
+    print('コピー先ファイルパス:${destFile.path}');
     try {
-      destinationFilePath.copySync(destinationFilePath.path);
+      sourceFile.copySync(destFile.path);
     } catch (e) {
       debugPrint('ファイルのコピー中にエラーが発生しました: $e');
     }
   }
 
   testWidgets('DBが空の状態でスタート', (WidgetTester tester) async {
-    // DBのSetUpをやるよ
+    // DBが存在すれば削除
+    await deleteFile();
+    addTearDown(() {
+      deleteFile();
+    });
+
     final r = Robot(tester);
     await r.pumpMyApp();
 
@@ -80,23 +85,34 @@ void main() {
     // 【消して良いやつ】>>>>>>>>>> ここまで >>>>>>>>>>
 
     // await todoRobot.slideTodo();
-
-    addTearDown(() {
-      // このテストケースの直後のみで実行される
-      debugPrint("   Only this Test Case");
-    });
   });
 
   testWidgets('DBにデータがある状態でスタート', (WidgetTester tester) async {
-    // DBのSetUpをやるよ
+    // テスト用DBの配置
+    await deleteFile();
+    await deployFile();
+    addTearDown(() {
+      deleteFile();
+    });
+
     final r = Robot(tester);
     await r.pumpMyApp();
+    await tester.pumpAndSettle(Duration(seconds: 2));
+
+    // 【消して良いやつ】<<<<<<<<< ここから <<<<<<<<<
+    // for (var i = 0; i < 12; i++) { // Todoをスクロールできるように１２個作る
+    // 試しに１個Todoを追加する操作
+    await r.mainScreen.pressAddButton();
+    await r.modalBottomSheet.enterTodoTitle('title');
+    await r.modalBottomSheet.pressRegisterButton();
+    // } // Todoをスクロールできるように１２個作る
+
+    await r.todoPane.scrollToBottom();
+    await r.todoPane.scrollToTop();
+
+    await r.mainScreen.changeComboBoxItem();
+    // 【消して良いやつ】>>>>>>>>>> ここまで >>>>>>>>>>
 
     // await todoRobot.slideTodo();
-
-    addTearDown(() {
-      // このテストケースの直後のみで実行される
-      debugPrint("   Only this Test Case");
-    });
   });
 }
